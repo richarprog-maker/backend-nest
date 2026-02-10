503b0b1c68219650003ad45533ba7b22c0a08fec
richarprog-maker <tu_email@example.com>
2026-02-10 16:59:10 -0500

    se corrige la ruta

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [
         {
             name: 'checor-backend',
-            script: 'dist/main.js',
+            script: 'dist/src/main.js',
             instances: 1,
             autorestart: true,
             watch: false,
@@ -15,7 +15,7 @@ module.exports = {
         },
         {
             name: 'checor-tasks',
-            script: 'dist/main.js', 
+            script: 'dist/src/main.js',
             instances: 1,
             autorestart: true,
             watch: false,
